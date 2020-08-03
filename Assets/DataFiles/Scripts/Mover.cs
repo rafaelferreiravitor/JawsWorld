a884a9c735f60d8a19a7eb9fc37b079b5070a8b5
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-08-03 08:01:01 -0300

    Click to draw a raycast. and bake the navmesh, obstacle objects and navmesh minimum area

--- Assets/DataFiles/Scripts/Mover.cs
+++ Assets/DataFiles/Scripts/Mover.cs
@@ -7,14 +7,23 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] GameObject target;
+
+    Ray lastRay;
+
     void Start()
     {
-        
+        GetComponent<NavMeshAgent>().speed *= 2;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin,lastRay.direction*100);
+        
     }
 }
