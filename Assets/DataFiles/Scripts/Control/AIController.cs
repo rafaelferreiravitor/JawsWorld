9b8b689f54eb7fdb3a56c3a2f2eb7c90ffd72242
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-08-23 06:15:14 -0300

    Added suspicious behaviour to enemies

--- Assets/DataFiles/Scripts/Control/AIController.cs
+++ Assets/DataFiles/Scripts/Control/AIController.cs
@@ -11,27 +11,58 @@ namespace RPG.Control
         [SerializeField] float chaseDistance = 5f;
         GameObject playerTarget;
         Fighter fighter;
+        Mover mover;
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        [SerializeField] float SuspiciousTime = 5f;
+
         private void Start()
         {
             playerTarget = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
+            guardPosition = transform.position;
+            mover = GetComponent<Mover>();
         }
 
         private void Update()
         {
             if (InAttackRangeOfPlayer())
             {
-                fighter.StartAction(playerTarget.GetComponent<Health>());
+                AttackBehaviour();
+            }
+            else if (Suspicious())
+            {
+                SuspiciousBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+        }
+
+        private bool Suspicious()
+        {
+            return Time.time - timeSinceLastSawPlayer <= SuspiciousTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartAction(guardPosition);
+        }
+
+        private void SuspiciousBehaviour()
+        {
+            fighter.Cancel();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.StartAction(playerTarget.GetComponent<Health>());
+            timeSinceLastSawPlayer = Time.time;
         }
 
         private bool InAttackRangeOfPlayer()
         {
-            
             return (Vector3.Distance(transform.position, playerTarget.transform.position) <= chaseDistance && fighter.CanAttack(playerTarget));
         }
 
